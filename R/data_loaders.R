1bf13fdab1b03f14a13cdd51336c05d75cc6ce70
Matthew Zatzman <matthew.zatzman@gmail.com>
2022-06-27 18:21:20 -0400

    tidy code

--- R/data_loaders.R
+++ R/data_loaders.R
@@ -26,11 +26,12 @@ read_vartrix <- function(dir_path = NULL, mtx_ref, mtx_alt, barcodes, variants) 
   snps <- read.table(variants, header = FALSE, col.names = "pos") %>%
     tidyr::separate(pos, into = c("chr", "pos"), sep = "_") %>%
     # Big memory savings plus we rebase the positions to match original VCF
-    dplyr::mutate("chr" = as.factor(chr), "pos" = as.integer(as.integer(pos)+1))
+    dplyr::mutate("chr" = as.factor(chr), "pos" = as.integer(as.integer(pos) + 1))
 
   colnames(ref) <- cells$barcodes
   colnames(alt) <- cells$barcodes
 
-  sce <- SingleCellExperiment(list(ref=ref, alt=alt), rowData = snps, colData = cells)
+  sce <- SingleCellExperiment(list(ref = ref, alt = alt), rowData = snps, colData = cells)
+
   return(sce)
 }
