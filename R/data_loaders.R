354e938054315d58b7d64565db48ec4bd0a693d1
Matthew Zatzman <matthew.zatzman@gmail.com>
2022-07-08 19:40:59 -0400

    Update to bin atac data loader

--- R/data_loaders.R
+++ R/data_loaders.R
@@ -1,18 +1,23 @@
 #' Load atac binned depth data
 #'
-#' Loads binned atac reads, merges cell-wise and bin-wise metadata, and performs QC,
+#' Loads binned atac reads, merges cell-wise and bin-wise metadata, and performs QC.
 #'
 #' @param directory Folder containing samples
 #' @param ArchR_Proj Optional: ArchR project with matching cells and metadata
 #' @param bins Optional: GRanges bins object
 #' @param BPPARAM Options to pass to `bplapply` for data loading. Provides minor speedup if many samples
+#' @param flag_ideal Logical. Whether or not to flag ideal bins for each cell. Can be slow if not parallelized. See [get_ideal_mat()] for more information.
+#' @param save_to File path in which to save the final output. Note: Will still return the sce object for downstream analysis.
+#' @param save_as NOT IMPLEMENTED YET. Select file formats to save the object. Can provide multiple values
+#'
+#' @inheritParams get_ideal_mat
 #'
 #' @return A `SingleCellExperiment` object.
 #' @export
 #'
-load_atac_bins <- function(directory, ArchR_Proj = NULL, bins = NULL, BPPARAM = bpparam()) {
-  sample_names = dir(directory)
-  samples = dir(directory, full.names = TRUE)
+load_atac_bins <- function(directory, ArchR_Proj = NULL, bins = NULL, BPPARAM = bpparam(), gc = NULL, n_freq = NULL, map = 1, min_reads = 1, max_N_freq = 0.05, reads_outlier = 0.01, gc_outlier = 0.001, min_map = 0.9, ncores = 1, flag_ideal = TRUE, save_to = NULL, save_as = c("sce", "adata", "seurat")) {
+  sample_names <- dir(directory)
+  samples <- dir(directory, full.names = TRUE)
 
   message("Loading bin counts")
   sce <- DropletUtils::read10xCounts(samples = samples, sample.names = sample_names, col.names = TRUE, BPPARAM = BPPARAM)
@@ -30,10 +35,10 @@ load_atac_bins <- function(directory, ArchR_Proj = NULL, bins = NULL, BPPARAM = 
     sce <- sce[, match(rownames(ArchR_Proj@cellColData), colnames(sce))]
 
     # Line up and merge
-    colData(sce) <- cbind(colData(sce), subset(proj@cellColData[colnames(sce),], select = -c(Sample)))
+    colData(sce) <- cbind(colData(sce), subset(ArchR_Proj@cellColData[colnames(sce), ], select = -c(Sample)))
 
     # Throw error if the merging is imperfect
-    stopifnot(all(sce$Barcode == rownames(proj@cellColData)))
+    stopifnot(all(sce$Barcode == rownames(ArchR_Proj@cellColData)))
   }
 
   # Merge bin level information if provided
@@ -42,17 +47,21 @@ load_atac_bins <- function(directory, ArchR_Proj = NULL, bins = NULL, BPPARAM = 
     rowRanges(sce) <- sort(GenomicRanges::makeGRangesFromDataFrame(merge(rowData(sce), as.data.frame(bins), by.x = "ID", by.y = "bin_id"), keep.extra.columns = TRUE))
     rownames(sce) <- rowData(sce)$ID
 
+    # Grab these values from the bins df
+    if (!is.null(bins$gc)) {
+      gc <- bins$gc
+    }
+    if (!is.null(bins$n_freq)) {
+      n_freq <- bins$n_freq
+    }
+
     # TODO: add bin arm information
     # rowRanges(sce)$chr_arm <- with(rowRanges(sce), interaction(seqnames, arm, sep = "", lex.order = T))
   }
 
-  # Perform initial QC
-  message("Adding cellwise and binwise QC metrics")
-  sce <- scuttle::addPerCellQCMetrics(sce)
-  sce <- scuttle::addPerFeatureQCMetrics(sce, subsets = get_f_idx(sce$Sample))
+  # Throw error if row names are not matching
+  stopifnot(all(rownames(sce) == rowData(sce)$ID))
 
-  message("Computing library size factors")
-  sce <- scuttle::computeLibraryFactors(sce)
 
   message("Performing bin-length normalization. Storing as assay(sce, 'counts_permb')")
   # Bin length normalize upfront
@@ -62,25 +71,47 @@ load_atac_bins <- function(directory, ArchR_Proj = NULL, bins = NULL, BPPARAM = 
   # Add binwidth if not provided
   if (is.null(rowData(sce)$binwidth)) {
     spl <- stringr::str_split(rowData(sce)$ID, "_", simplify = T)
-    rowData(sce)$binwidth = as.numeric(spl[,3]) - as.numeric(spl[,2]) + 1
+    rowData(sce)$binwidth <- as.numeric(spl[, 3]) - as.numeric(spl[, 2]) + 1
   }
 
   assay(sce, "counts_permb") <- round(assay(sce, "raw_counts") / (rowData(sce)$binwidth / 1e6), 2)
 
+  # Perform initial QC
+  message("Adding cellwise and binwise QC metrics")
+  sce <- scuttle::addPerCellQCMetrics(sce)
+  sce <- scuttle::addPerFeatureQCMetrics(sce, subsets = get_f_idx(sce$Sample))
+
   # Add valid and ideal information to mirror HMMcopy's QC
-  # Valid if >0 reads and nonegative GC (but we don't use the -1 stuff)
-  # ideal if not within the outliers of valid data wrt mappability, reads and gc content
+  if (flag_ideal) {
+    message("Flagging ideal bins using ", ncores, " threads")
+    assay(sce, "ideal_bins") <- get_ideal_mat(
+      mat = assay(sce, "counts_permb"),
+      gc = gc,
+      n_freq = n_freq,
+      map = map,
+      min_reads = min_reads,
+      max_N_freq = max_N_freq,
+      reads_outlier = reads_outlier,
+      gc_outlier = gc_outlier,
+      min_map = min_map,
+      ncores = ncores
+    )
+  }
 
+  message("Computing library size factors")
+  sce <- scuttle::computeLibraryFactors(sce)
 
-  # Throw error if row names are not matching
-  stopifnot(all(rownames(sce) == rowData(sce)$ID))
-
+  cat("\n")
   print(sce)
+
+  if (!is.null(save_to)) {
+    dir.create(dirname(save_to), recursive = TRUE, showWarnings = FALSE)
+    cat("\nSaving sce object to ", '"', save_to, '"', "\n", sep = "")
+    save(sce, file = save_to)
+  }
 
   return(sce)
 }
-
-
 
 
 #' Read Vatrix Data
