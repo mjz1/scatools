34fdd23d6c3dc86bececc23012a854247cbabc0e
Matthew Zatzman <matthew.zatzman@gmail.com>
2022-07-28 18:45:07 -0400

    Avoid using bins df in plot cell cna

--- R/plotting.R
+++ R/plotting.R
@@ -1,13 +1,23 @@
 
 #' @export
-plot_cell_cna <- function(sce, cell_id, assay_name = "counts") {
+plot_cell_cna <- function(sce, cell_id = NULL, assay_name = "counts") {
+  if (is.null(cell_id)) {
+    logger::log_warn("No cell ids provided! Plotting all cells. Are you sure you want to do this!?")
+    cell_id = colnames(sce)
+  }
+
+  # TODO: Try to intelligently find the start positions from the provided sce
+  bindat <- as.data.frame(SummarizedExperiment::rowRanges(sce))
+  bindat$bin_id <- rownames(sce)
+
   makePerCellDF(sce[, cell_id], features = rownames(sce), assay.type = assay_name, use.coldata = F) %>%
     rownames_to_column(var = "barcode") %>%
-    pivot_longer(cols = starts_with("chr"), names_to = "bin_id", values_to = "counts") %>%
-    left_join(as.data.frame(bins)) %>%
+    pivot_longer(cols = dplyr::starts_with("chr"), names_to = "bin_id", values_to = "counts") %>%
+    left_join(as.data.frame(bindat)) %>%
     ggplot() +
     geom_point(aes(x = start, y = counts), size = 0.5) +
     facet_grid(barcode ~ seqnames, scales = "free_x", space = "free_x") +
+    labs(x = NULL, y = assay_name) +
     theme(
       panel.spacing = unit(0, "lines"),
       panel.border = element_rect(fill = NA),
