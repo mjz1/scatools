0bc7743b319e6ad5d3c6b834c8663b29864112b0
Matthew Zatzman <matthew.zatzman@gmail.com>
2022-07-28 23:40:25 -0400

    plotting sc updates

--- R/plotting.R
+++ R/plotting.R
@@ -8,15 +8,33 @@ plot_cell_cna <- function(sce, cell_id = NULL, assay_name = "counts") {
 
   # TODO: Try to intelligently find the start positions from the provided sce
   bindat <- as.data.frame(SummarizedExperiment::rowRanges(sce))
+  if (nrow(bindat) == 0) {
+    logger::log_warn("Bin data not found in rowRanges(sce). Checking rowData(sce)")
+    bindat <- as.data.frame(SummarizedExperiment::rowData(sce))
+    if (nrow(bindat) == 0) {
+      logger::log_error("Bin data not found in rowData(sce).")
+    }
+  } else {
+    # This means we do have rowranges.
+    # make sure seqnames are labelled as chr
+    bindat$chr <- bindat$seqnames
+  }
+  if (is.null(rownames(sce))) {
+    logger::log_error("rownames(sce) cannot be NULL.")
+  }
   bindat$bin_id <- rownames(sce)
 
-  makePerCellDF(sce[, cell_id], features = rownames(sce), assay.type = assay_name, use.coldata = F) %>%
+  # TODO: subset for main chromosomes and reorder)
+
+  plot_dat <- makePerCellDF(sce[, cell_id], features = rownames(sce), assay.type = assay_name, use.coldata = F) %>%
     rownames_to_column(var = "barcode") %>%
-    pivot_longer(cols = dplyr::starts_with("chr"), names_to = "bin_id", values_to = "counts") %>%
-    left_join(as.data.frame(bindat)) %>%
-    ggplot() +
+    pivot_longer(cols = -dplyr::starts_with("barcode"), names_to = "bin_id", values_to = "counts") %>%
+    left_join(as.data.frame(bindat))
+
+
+  ggplot(plot_dat) +
     geom_point(aes(x = start, y = counts), size = 0.5) +
-    facet_grid(barcode ~ seqnames, scales = "free_x", space = "free_x") +
+    facet_grid(barcode ~ chr, scales = "free_x", space = "free_x") +
     labs(x = NULL, y = assay_name) +
     theme(
       panel.spacing = unit(0, "lines"),
