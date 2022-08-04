386f09efd5eabeb5028f8935d7a0b259522ce7a9
Matthew Zatzman <matthew.zatzman@gmail.com>
2022-08-04 11:43:07 -0400

    colors initial commit

---
+++ R/colors.R
@@ -0,0 +1,11 @@
+
+state_cn_colors <- function() {
+  cn_colors <- structure(
+    c(
+      "#3182BD", "#9ECAE1", "#CCCCCC", "#FDCC8A", "#FC8D59", "#E34A33",
+      "#B30000", "#980043", "#DD1C77", "#DF65B0", "#C994C7", "#D4B9DA"
+    ),
+    names = c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11+")
+  )
+  return(cn_colors)
+}
