34a87fa2103e73b43c78ce40f49fa6acb98c0fa7
Matthew Zatzman <matthew.zatzman@gmail.com>
2022-09-09 04:35:37 -0400

    gc modal NA filtering

---
+++ R/gc_modal_filter.R
@@ -0,0 +1,23 @@
+#' GC modal QC filter
+#'
+#' Removes cells that have high numbers of NA bins after GC modal correction
+#'
+#' @param sce SingleCellExperiment object
+#' @param assay Assay containing GC modal corrected counts
+#' @param filter_prop Filter proportion
+#' @param verbose Logical. Message verbosity
+#'
+#' @return Filtered SingleCellExperiment
+#' @export
+#'
+gc_modal_qc_filter <- function(sce, assay = "counts_gc_modal", filter_prop = 0.05, verbose = TRUE) {
+  prop_na <- colSums(apply(assay(sce, assay), 2, is.na)) / nrow(samp_sce)
+
+  sce[["prop_na"]] <- prop_na
+
+  keep_cells <- which(prop_na <= filter_prop)
+
+  logger::log_info("Keeping {length(keep_cells)} of {ncol(sce)} cells: {signif(length(keep_cells) / ncol(sce) * 100, 3)}% (filter threshold: {filter_prop})")
+
+  return(sce[,keep_cells])
+}
