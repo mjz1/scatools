c28e29222e3ee1817468e55dafaaa5ea16d163da
Matthew Zatzman <matthew.zatzman@gmail.com>
2022-09-09 12:16:22 -0400

    chr arm aggregation cleaner and now able to integrate copy number signal from ground truth

--- R/summarise_chr_arm.R
+++ R/summarise_chr_arm.R
@@ -1,14 +1,18 @@
-#' Summarise copu number signal over chromosome arms
+#' Summarise copy number signal over chromosome arms
 #'
 #' @param sce SingleCellExperiment objet
 #' @param assay_name Name of assay
 #' @param genome_ver Genome version ("hg38" or "hg19")
+#' @param cn_granges Optional: Copy number granges to integrate into the arm level bins
+#' @param granges_signal_colname Optional: Name of the column containing the CNA information in `cn_granges`
 #'
 #' @return A chromosome arm level binned SingleCellExperiment
 #' @export
 #'
-# TODO: Make this more general to aggregate signal over defined bins
-summarise_chr_arm <- function(sce, assay_name, genome_ver = "hg38") {
+summarise_chr_arm <- function(sce, assay_name, cn_granges = NULL, granges_signal_colname = NULL, genome_ver = "hg38") {
+
+  # TODO: Make this more general to aggregate signal over defined bins
+
   chr_arm_granges <- get_chr_arm_bins(genome = genome_ver)
 
   y <- rowRanges(sce)
@@ -16,7 +20,15 @@ summarise_chr_arm <- function(sce, assay_name, genome_ver = "hg38") {
 
   chr_arm_granges <- integrate_segments(x = chr_arm_granges, y, granges_signal_colname = colnames(sce))
 
-  arm_sce <- SingleCellExperiment(assays = list("counts" = as.matrix(mcols(chr_arm_granges)[colnames(sce)])), rowRanges = chr_arm_granges, colData = colData(sce))
+  # Split out the count matrix from the ranges data
+  counts <- as.matrix(mcols(chr_arm_granges)[colnames(sce)])
+  mcols(chr_arm_granges)[colnames(sce)] <- NULL # Remove all the matrix columns
+
+  arm_sce <- SingleCellExperiment(assays = list("counts" = counts), rowRanges = chr_arm_granges, colData = colData(sce))
+
+  if (!is.null(cn_granges)) {
+    rowRanges(arm_sce) <- integrate_segments(x = rowRanges(arm_sce), y = cn_granges, granges_signal_colname = granges_signal_colname, drop_na = FALSE)
+  }
 
   # Silly workaround
   assay(arm_sce, assay_name) <- assay(arm_sce, "counts")
