4f8812be17400edd18aec6ffe842e849581c7cb2
Matthew Zatzman <matthew.zatzman@gmail.com>
2022-12-01 22:09:44 -0500

    filter_sce bugfix

--- R/quality_control.R
+++ R/quality_control.R
@@ -4,7 +4,7 @@
 #'
 #' @param sce SingleCellExperiment object
 #' @param assay_name Name of assay to pull counts from. Ideally raw counts
-#' @param which Filter cells or bins
+#' @param filt Filter cells or bins
 #' @param min_counts Minimum counts per cell/bin
 #' @param min_prop Minimum proportion of cells/bins with `min_counts`
 #' @param flag_only Only flag cells/bins, do not remove
@@ -27,7 +27,7 @@ filter_sce <- function(sce, assay_name = "counts", which = c("bins", "cells"), m
   for (f in which) {
     if (f == "bins") {
       m = 1
-      bin_bool <- (colSums(apply(X = assay(sce, assay_name), MARGIN = m, function(x) x >= min_counts)) / ncol(sce)) >= min_prop
+      bin_bool <- (colSums(apply(X = assay(sce, assay_name), MARGIN = m, function(X) X >= min_counts)) / ncol(sce)) >= min_prop
 
       # Flag
       rowData(sce)[[paste0("keep_", f)]] <- bin_bool
@@ -37,22 +37,23 @@ filter_sce <- function(sce, assay_name = "counts", which = c("bins", "cells"), m
       if (!flag_only) {
         sce <- sce[bin_bool,]
       }
-
     }
+
     if (f == "cells") {
       m = 2
-      cell_bool <- (colSums(apply(X = assay(sce, assay_name), MARGIN = m, function(x) x >= min_counts)) / nrow(sce)) >= min_prop
+      cell_bool <- (colSums(apply(X = assay(sce, assay_name), MARGIN = m, function(X) X >= min_counts)) / nrow(sce)) >= min_prop
 
       # Flag
       colData(sce)[[paste0("keep_", f)]] <- cell_bool
 
       logger::log_info("Keeping {sum(cell_bool)} of {length(cell_bool)} cells")
 
+      if (!flag_only) {
+        sce <- sce[,which(cell_bool)]
+      }
+
     }
 
-    if (!flag_only) {
-      sce <- sce[,cell_bool]
-    }
 
   }
 
