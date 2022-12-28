5efbc421a3b7dee4c6d17b102653404037076394
Matthew Zatzman <matthew.zatzman@gmail.com>
2022-12-28 16:31:02 -0500

    contain archr working directory using `withr` and optional save_h5ad

--- R/run_scatools.R
+++ R/run_scatools.R
@@ -3,26 +3,28 @@
 #' Convenience wrapper to run a sample
 #'
 #' @param sample_id Sample id
+#' @param bins Bins granges
 #' @param fragments Fragments file
 #' @param ArrowFile ArrowFile
 #' @param outdir Outdir
 #' @param overwrite Whether to overwrite previous results
 #' @param force_arrow recreate arrow file
 #' @param verbose Verbosity
-#' @param bins Bins granges
+#' @param save_h5ad Logical. Whether to save raw and processed h5ad files. Requires packages `zellkonverter` and `anndata`
 #' @param ncores Number of cores
 #'
 #' @return `SingleCellExperiment` object
 #' @export
 #'
 run_scatools <- function(sample_id,
+                         bins,
                          fragments = NULL,
                          ArrowFile = NULL,
                          outdir = sample_id,
                          overwrite = FALSE,
                          force_arrow = FALSE,
                          verbose = TRUE,
-                         bins,
+                         save_h5ad = TRUE,
                          ncores = 1) {
 
   # TODO INPUT VALIDATION
@@ -119,15 +121,16 @@ run_scatools <- function(sample_id,
   save_to(object = sce_processed, save_to = final_out)
 
   # Save anndata
-  if (requireNamespace("zellkonverter", quietly = T) & requireNamespace("anndata")) {
-    logger::log_info("Writing output to anndata")
-    # Save raw anndata
-    adata <- zellkonverter::SCE2AnnData(sce)
-    anndata::write_h5ad(adata, filename = file.path(processed, glue::glue("{bin_name}_raw.h5ad")))
-    adata2 <- zellkonverter::SCE2AnnData(sce_processed, verbose = TRUE, reducedDims = FALSE) # Bug in reduced dims conversion nbd
-    anndata::write_h5ad(adata2, filename = file.path(processed, glue::glue("{bin_name}_processed.h5ad")))
+  if (save_h5ad == TRUE) {
+    if (requireNamespace("zellkonverter", quietly = T) & requireNamespace("anndata")) {
+      logger::log_info("Writing output to anndata")
+      # Save raw anndata
+      adata <- zellkonverter::SCE2AnnData(sce)
+      anndata::write_h5ad(adata, filename = file.path(processed, glue::glue("{bin_name}_raw.h5ad")))
+      adata2 <- zellkonverter::SCE2AnnData(sce_processed, verbose = TRUE, reducedDims = FALSE) # Bug in reduced dims conversion nbd
+      anndata::write_h5ad(adata2, filename = file.path(processed, glue::glue("{bin_name}_processed.h5ad")))
+    }
   }
-
   return(sce_processed)
 }
 
@@ -143,19 +146,46 @@ create_arrow_file <- function(fragments = NULL, ArrowFile, archr_dir, sample_id 
   ArchR::addArchRGenome(genome)
 
   dir.create(archr_dir, recursive = TRUE, showWarnings = FALSE)
-  # Set working directory for ArchR
-  # knitr::opts_current$set(root.dir = archr_dir)
-  setwd(archr_dir)
-  if (file.exists(ArrowFile) & !force) {
-    logger::log_info("LOADING ARROWFILE")
-    proj <- ArchR::ArchRProject(
-      ArrowFile = ArrowFile,
-      showLogo = FALSE,
-      outputDirectory = archr_dir,
-      copyArrows = FALSE # This is recommended so that you maintain an unaltered copy for later usage.
-    )
-    # Just in case no doublet calls
-    if (is.null(proj$DoubletEnrichment)) {
+
+  # Set working directory for ArchR temporarily
+  withr::with_dir(archr_dir, code = {
+    if (file.exists(ArrowFile) & !force) {
+      logger::log_info("LOADING ARROWFILE")
+      proj <- ArchR::ArchRProject(
+        ArrowFile = ArrowFile,
+        showLogo = FALSE,
+        outputDirectory = archr_dir,
+        copyArrows = FALSE # This is recommended so that you maintain an unaltered copy for later usage.
+      )
+      # Just in case no doublet calls
+      if (is.null(proj$DoubletEnrichment)) {
+        logger::log_info("CALCULATING DOUBLETS")
+        doubScores <- ArchR::addDoubletScores(
+          input = ArrowFile,
+          k = 10, # Refers to how many cells near a "pseudo-doublet" to count.
+          knnMethod = "LSI", # Refers to the embedding to use for nearest neighbor search.
+          LSIMethod = 1,
+          force = FALSE
+        )
+        # Reload
+        proj <- ArchR::ArchRProject(
+          ArrowFile = ArrowFile,
+          outputDirectory = archr_dir,
+          copyArrows = FALSE # This is recommended so that you maintain an unaltered copy for later usage.
+        )
+      }
+    } else {
+      logger::log_info("CREATING ARROWFILE")
+      logger::log_info("Working in: ", getwd())
+      ArrowFile <- ArchR::createArrowFiles(
+        inputFiles = fragments,
+        sampleNames = sample_id,
+        minTSS = 4, # Dont set this too high because you can always increase later
+        minFrags = 1000,
+        addTileMat = TRUE,
+        addGeneScoreMat = TRUE,
+        force = force
+      )
       logger::log_info("CALCULATING DOUBLETS")
       doubScores <- ArchR::addDoubletScores(
         input = ArrowFile,
@@ -164,39 +194,14 @@ create_arrow_file <- function(fragments = NULL, ArrowFile, archr_dir, sample_id 
         LSIMethod = 1,
         force = FALSE
       )
-      # Reload
       proj <- ArchR::ArchRProject(
         ArrowFile = ArrowFile,
         outputDirectory = archr_dir,
         copyArrows = FALSE # This is recommended so that you maintain an unaltered copy for later usage.
       )
     }
-  } else {
-    logger::log_info("CREATING ARROWFILE")
-    logger::log_info("Working in: ", getwd())
-    ArrowFile <- ArchR::createArrowFiles(
-      inputFiles = fragments,
-      sampleNames = sample_id,
-      minTSS = 4, # Dont set this too high because you can always increase later
-      minFrags = 1000,
-      addTileMat = TRUE,
-      addGeneScoreMat = TRUE,
-      force = force
-    )
-    logger::log_info("CALCULATING DOUBLETS")
-    doubScores <- ArchR::addDoubletScores(
-      input = ArrowFile,
-      k = 10, # Refers to how many cells near a "pseudo-doublet" to count.
-      knnMethod = "LSI", # Refers to the embedding to use for nearest neighbor search.
-      LSIMethod = 1,
-      force = FALSE
-    )
-    proj <- ArchR::ArchRProject(
-      ArrowFile = ArrowFile,
-      outputDirectory = archr_dir,
-      copyArrows = FALSE # This is recommended so that you maintain an unaltered copy for later usage.
-    )
-  }
+  })
+
   return(proj)
 }
 
