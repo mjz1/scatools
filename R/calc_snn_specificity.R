f616575dd9e3d0c8d05b4942f4cdedf28259c6da
Matthew Zatzman <matthew.zatzman@gmail.com>
2023-06-16 10:18:42 -0400

    Weighted cluster purity calculation added

--- R/calc_snn_specificity.R
+++ R/calc_snn_specificity.R
@@ -1,10 +1,13 @@
 #' Calculate SNN Specificity
 #' 
-#' Calculates the fraction of nearest neighbours divided by the expected fraction of nearest neighbours in the patient subgraph.
+#' Calculates the fraction of nearest neighbours divided by the expected fraction of nearest neighbours in the patient subgraph. 
+#' 
+#' Also computes the neighbor purity estimation for each identity.
 #'
 #' @param snn_graph A SNN matrix
 #' @param cell_idents1 Vector of cell identities for which the specificity will be calculated against. Ideally sample or patient.
 #' @param cell_idents2 Vector of cell identities for which define cell types or states. The variable for which we want to see if the factor is specific to cell_idents1.
+#' @param weighted Logical. Whether to weight the identities by their inverse size. Only relevent for the neighbor purity estimated
 #' @param sampling_rate Proportion of cells to subsample when performing calculations. 
 #' @param ncores Number of cores to use
 #'  
@@ -12,7 +15,12 @@
 #' @return A dataframe
 #' @export
 #'
-calc_snn_specificity <- function(snn_graph, cell_idents1, cell_idents2, sampling_rate = 0.1, ncores = 1) {
+calc_snn_specificity <- function(snn_graph, 
+                                 cell_idents1, 
+                                 cell_idents2, 
+                                 sampling_rate = 0.1, 
+                                 weighted = T,
+                                 ncores = 1) {
   # Refactored from: /work/shah/users/uhlitzf/projects/spectrum_explore/140_SPECTRUM_freeze_patient_mixing.Rmd
 
   ### For testing ###
@@ -36,6 +44,18 @@ calc_snn_specificity <- function(snn_graph, cell_idents1, cell_idents2, sampling
 
   cell_idents1 <- as.factor(cell_idents1)
   cell_idents2 <- as.factor(cell_idents2)
+  
+  if (weighted == TRUE) {
+    # Weight inversely by size of each identity
+    c1_w <- 1/table(cell_idents1)
+    c1_w <- as.numeric(c1_w[as.character(cell_idents1)])
+    
+    c2_w <- 1/table(cell_idents2)
+    c2_w <- as.numeric(c2_w[as.character(cell_idents2)])
+  } else {
+    c1_w <- c2_w <- rep(1, length(cell_idents1))
+  }
+
   
   # Compute the fraction of cell_idents2 from each cell_idents1
   cell_idents1_freq <- as.data.frame.matrix(prop.table(table(cell_idents1, cell_idents2), margin = 2)) %>%
@@ -64,11 +84,17 @@ calc_snn_specificity <- function(snn_graph, cell_idents1, cell_idents2, sampling
     # Get neighbors of the current cell
     snn_idx <- which(snn_graph_test[,cell_id] != 0)
     
+    snn <- snn_graph_test[snn_idx,cell_id]
+    
+    # Get the weights when summing the SNN
+    c1_snn_w <- c1_w[snn_idx]
+    c2_snn_w <- c2_w[snn_idx]
+    
     # Identities of the neighbors for the cell being tested
     nb_cid1 <- cell_idents1[snn_idx]
     nb_cid2 <- cell_idents2[snn_idx]
     
-    return(data.frame(nb_cid1, nb_cid2))
+    return(data.frame(snn, c1_snn_w, c2_snn_w, nb_cid1, nb_cid2))
   })
   
   res_df <- purrr::list_rbind(res, names_to = "idx") %>%
@@ -76,14 +102,33 @@ calc_snn_specificity <- function(snn_graph, cell_idents1, cell_idents2, sampling
                   cid1 = cell_idents1_test[idx],
                   cid2 = cell_idents2_test[idx], .after = "idx") %>%
     dplyr::left_join(cell_idents1_freq, by = c("cid1" = "cell_idents1", "cid2" = "cell_idents2")) %>%
-    dplyr::group_by(cell_id) %>%
+    dplyr::mutate(cid1_match = (cid1 == nb_cid1),
+           cid2_match = (cid2 == nb_cid2)) %>%
+  dplyr::group_by(cell_id) %>%
     dplyr::mutate(
       nexp = nrel * length(cell_id),
       nobs = sum(cid1 == nb_cid1),
       score = log2(nobs / (nexp + 1)),
-      score2 = log2(nobs / nexp)
-    ) %>%
-    dplyr::distinct(cell_id, cid2, nexp, nobs, nrel, score, score2)
+      score2 = log2(nobs / nexp),
+      cid1_w_total = sum(c1_snn_w),
+      cid2_w_total= sum(c2_snn_w),
+      cid1_w_target = sum(c1_snn_w * cid1_match),
+      cid2_w_target = sum(c2_snn_w * cid2_match)) %>%
+    dplyr::distinct(cell_id, 
+                    cid1, 
+                    cid2, 
+                    nexp, 
+                    nobs, 
+                    nrel, 
+                    score, 
+                    score2, 
+                    cid1_w_total, 
+                    cid1_w_target,
+                    cid2_w_total,
+                    cid2_w_target) %>%
+    dplyr::mutate(cid1_purity = cid1_w_target / cid1_w_total,
+                  cid2_purity = cid2_w_target / cid2_w_total)
+  return(res_df)
 }
 
 
