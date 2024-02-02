8e3baf3dd36c4655ed1e42af484ff59d57cef9b2
Matthew Zatzman <matthew.zatzman@gmail.com>
2024-02-02 09:55:31 -0500

    cell density code

---
+++ R/compute_cell_density.R
@@ -0,0 +1,14 @@
+.get_density <- function(x, y, ...) {
+  dens <- MASS::kde2d(x, y, ...)
+  ix <- findInterval(x, dens$x)
+  iy <- findInterval(y, dens$y)
+  ii <- cbind(ix, iy)
+  return(dens$z[ii])
+}
+
+
+.normalize <- function(x, na.rm = TRUE) {
+  return((x- min(x)) /(max(x)-min(x)))
+}
+
+
